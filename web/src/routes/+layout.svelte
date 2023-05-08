75bf70644fe8d197063759cf65907adb20a3256b
Jarda F <jarda@jarda.info>
2023-05-08 11:32:17 +0200

    show tz warning

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -3,12 +3,20 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		const hasTz = document.cookie.split('; ').find((c) => c.startsWith('tz='));
+		const currentTz = document.cookie
+			.split('; ')
+			.filter((c) => c.startsWith('tz='))
+			.map((c) => c.substring(3))[0];
+
 		const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 		document.cookie = `tz=${tz};max-age=31536000;path=/;samesite=strict`;
 
-		if (!hasTz) {
+		if (tz !== currentTz) {
+			alert(
+				`Your timezone appears to be "${tz}".\nThe page will now refresh to show dates in your local timezone.`
+			);
+
 			window.location.reload();
 		}
 	});
