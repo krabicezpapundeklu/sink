a9e16af708fdb69cb40f8683681da4bb9ccbb15e
JF <jarda@jarda.info>
2024-02-02 12:50:58 +0100

    skip linenoise build on windows

--- build.rs
+++ build.rs
@@ -1,25 +1,34 @@
-use std::io::Result;
+use std::{env::var_os, io::Result};
 
 use cc::Build;
 use static_files::NpmBuild;
 
 fn build_sqlite_shell() {
-    println!("cargo:rerun-if-changed=linenoise");
     println!("cargo:rerun-if-changed=sqlite-shell");
 
-    Build::new()
-        .define("HAVE_LINENOISE", None)
-        .files(["./linenoise/linenoise.c", "./sqlite-shell/shell.c"])
+    let mut build = Build::new();
+
+    build
+        .file("./sqlite-shell/shell.c")
+        .flag_if_supported("-Wno-cast-function-type")
         .flag_if_supported("-Wno-implicit-fallthrough")
         .flag_if_supported("-Wno-unused-function")
-        .flag_if_supported("-Wno-sign-compare")
-        .include("./linenoise")
-        .compile("sqlite-shell");
+        .flag_if_supported("-Wno-unused-variable")
+        .flag_if_supported("-Wno-sign-compare");
+
+    if var_os("CARGO_CFG_UNIX").is_some() {
+        println!("cargo:rerun-if-changed=linenoise");
+
+        build
+            .define("HAVE_LINENOISE", None)
+            .file("./linenoise/linenoise.c")
+            .include("./linenoise");
+    }
+
+    build.compile("sqlite-shell");
 }
 
-fn main() -> Result<()> {
-    build_sqlite_shell();
-
+fn build_web() -> Result<()> {
     println!("cargo:rerun-if-changed=event.types.json");
     println!("cargo:rerun-if-changed=item.types.json");
 
@@ -37,4 +46,9 @@ fn main() -> Result<()> {
         .target("web/build")
         .to_resource_dir()
         .build()
+}
+
+fn main() -> Result<()> {
+    build_sqlite_shell();
+    build_web()
 }
