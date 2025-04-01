514b5d663da060121823720ecaccf87a474f0628
Jarda F <jarda@jarda.info>
2025-04-01 13:55:25 +0200

    fix migration

--- migrations/20241231115425_update_item_header_item_id_to_not_null.sql
+++ migrations/20241231115425_update_item_header_item_id_to_not_null.sql
@@ -1,5 +1,8 @@
 CREATE TABLE IF NOT EXISTS item_header_new (item_id INTEGER NOT NULL REFERENCES item (id) ON DELETE CASCADE, name TEXT NOT NULL, value BLOB NOT NULL) STRICT;
 
+DELETE FROM item_body WHERE item_id NOT IN (SELECT id FROM item);
+DELETE FROM item_header WHERE item_id NOT IN (SELECT id FROM item);
+
 INSERT INTO item_header_new (item_id, name, value) SELECT item_id, name, value FROM item_header;
 
 DROP TABLE item_header;
